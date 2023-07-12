f1e06faae65f1ad031bb795dbc565f02578bb750
gyandeep <gyandeepsharma007@gmail.com>
2023-07-12 19:58:04 +0530

    changes return string

--- src/main/java/com/example/hellodeploymentapp/controller/HelloController.java
+++ src/main/java/com/example/hellodeploymentapp/controller/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping(value = "/hello")
     public String helloController(@RequestParam(required = false) String name){
-        return (name == null || name.isEmpty())?"Welcome user":"welcome "+name;
+        return (name == null || name.isEmpty())?"Welcome dummy user":"welcome "+name;
     }
 }
